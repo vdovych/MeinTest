5d47d81f8549c3d33ca864684234999beb8f6a0c
Станіслав Вдович < >
2016-12-20 15:35:50 +0200

    first commit

---
+++ ap.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hi():
+    return render_template('main.html')
+
+
+@app.route('/reg', methods=['POST'])
+def reg():
+    if (request.form('Name') == ''):
+        return 'Fail'
+    return 'Registration will be here'
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True, host='0.0.0.0', port=8080)
